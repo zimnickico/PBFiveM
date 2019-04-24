d875c0de1b70d0afd5a351e2ad314c8b159f8e97
Chris <47279613+actuallyitschris@users.noreply.github.com>
2019-04-24 19:38:26 +0200

    Add files via upload

---
+++ PB/__resource.lua
@@ -0,0 +1,5 @@
+description 'Over complicated FiveM Panic Button Script.'
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937.'
+
+client_script "client.lua"
---
+++ PB/client.lua
@@ -0,0 +1,80 @@
+--------------------------------
+------ PANIC BUTTON SCRIPT -----
+--------- MADE BY CHRIS --------
+
+blip = nil
+blips = {}
+
+-- Intial
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if IsControlPressed(1, 19) and IsControlJustPressed(1, 46) then
+
+local location = GetStreetNameAtCoord(playerPos, streetName, crossingRoad)
+local location2 = GetStreetNameFromHashKey(location)
+print (playerPos)
+print (location2)
+
+local name = GetPlayerName(PlayerId())
+Info()
+Citizen.Wait(100)
+Info2(location2)
+PlaySound(-1, "5_Second_Timer", "DLC_HEISTS_GENERAL_FRONTEND_SOUNDS", 0);
+
+ local name = GetPlayerName(PlayerId())
+local ped = GetPlayerPed(-1)
+local playerPos = GetEntityCoords(ped, true)
+print (playerPos)
+
+-- Blip
+
+blip = AddBlipForCoord (playerPos)
+SetBlipSprite(blip, 459)
+SetBlipScale(blip, 1.5)
+SetBlipColour(blip, 1)
+ BeginTextCommandSetBlipName("STRING")
+ AddTextComponentString('Panic Button by ' .. name)
+ EndTextCommandSetBlipName(blip)
+ table.insert(blips, blip)
+     Wait(60000)
+    for i, blip in pairs(blips) do
+        RemoveBlip(blip)
+    end
+
+end
+end
+end)
+
+-- Info
+
+function Info(text)
+    local txd = "CHAR_CALL911"
+
+    SetNotificationTextEntry("STRING")
+    SetNotificationFlashColor(6)
+    SetNotificationBackgroundColor(6)
+    AddTextComponentSubstringPlayerName('An officer has activated his Panic Button!')
+
+    -- Set the notification icon, title and subtitle.
+    local title = GetPlayerName(PlayerId())
+    local subtitle = "Panic Button"
+    local iconType = 0
+    local flash = false
+    SetNotificationMessage(txd, txd, flash, iconType, title, subtitle)
+    DrawNotification(true, true)
+end
+
+function Info2(text)
+	SetNotificationTextEntry("STRING")
+	AddTextComponentSubstringPlayerName('Respond to ' .. text)
+  SetNotificationFlashColor(6)
+  SetNotificationBackgroundColor(6)
+	DrawNotification(true, true)
+end
+
+
+--- YOU MAY MODIFY THE CODE ABOVE, ---
+--- ALTHOUGH YOU MAY NOT REALESE IT ---
+--- WITHOUT MY EXPLICIT PERMISSION ---
